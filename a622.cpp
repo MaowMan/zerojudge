578f19057237a5384a4726f861f10e2a151356b1
MaowMan <54308739+MaowMan@users.noreply.github.com>
2020-01-29 13:59:40 +0800

    Update a622.cpp

--- a622.cpp
+++ a622.cpp
@@ -1,37 +1,44 @@
-#include <vector>
-#include <string>
 #include <iostream>
-
-int main(){
-    std::vector<std::string> data;
-    std::string str;
-    std::string blank;
-    int maxlen = -1;
-    while(std::getline(std::cin,str)){
-        if(str=="END"){
-            data.pop_back();
-            std::cout<<"maxlen:"<<maxlen<<std::endl;
-            for(int i=0;i<maxlen;i++){
-                for(int k=0;k<data.size();k++){
-                    if(i>=data[k].size()){
-                        std::cout<<' ';
-                    }
-                    else{
-                        std::cout<<data[k][i];
-                    }
-                }
-                std::cout<<std::endl;
-            }
-            return 0;
-        }
-        else{
-            std::cout<<str.size()<<std::endl;
-            if(str.size()>maxlen){
-                std::cout<<"yep"<<std::endl;
-                maxlen=str.size();
-            }
-            data.push_back(str);
-            data.push_back(blank);
+#include <string>
+#include <vector>
+#include <cstdio>
+void print(std::vector<std::string> &data){
+    int maxlen=0;
+    for(int i=0;i<data.size();i++){
+        if(data[i].size()>maxlen){
+            maxlen=data[i].size();
         }
     }
+    //std::cout<<"maxlen:"<<maxlen<<std::endl;
+    for(int i=0;i<maxlen;i++){
+        for(int k=0;k<data.size();k++){
+            if(i>data[k].size()-1){
+                putchar(' ');
+            }
+            else{
+                putchar(data[k][i]);
+            }
+            if(k==data.size()-1){
+                putchar('\n');
+            }
+            else{
+                putchar(' ');
+                putchar(' ');
+            }
+        }
+    }
+}
+
+
+int main(){
+    std::string str;
+    std::vector<std::string> data;
+    while(std::getline(std::cin,str)){
+        if(str=="END"){
+            break;
+        }
+        data.push_back(str);
+    }
+    print(data);
+    return 0;
 }
