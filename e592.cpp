add10a48f289ea19d78cb47168ca45ee2389fe1b
MaowMan <54308739+MaowMan@users.noreply.github.com>
2020-01-29 17:48:37 +0800

    Create e592.cpp

---
+++ e592.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main(){
+    int n;
+    std::cin>>n;
+    for(int i=0;i<n;i++){
+        int k;
+        std::cin>>k;
+        std::vector<std::string> names(k);
+        for(int r=0;r<k;r++){
+            std::string str;
+            names[r]=std::getline(std::cin,str);
+        }
+        std::vector<std::vector <int> > seq()
+        for(int r=0;r<k;r++)
+
+    }
+}
